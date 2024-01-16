ac17596450b7696697a02b3df9052245eae490a0
liat g <liatg812@gmail.com>
2024-01-15 22:42:06 -0500

    more changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
-import ReactDOM from 'react-dom/client'
-import React from 'react'
-import App from './App.jsx'
+import App from './App.jsx';
+import ReactDOM from 'react-dom/client';
+import React from 'react';
 import './index.css';
 
 
