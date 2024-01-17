990bc355262ebbd881dc098c14c3086e2da5af1d
liat g <liatg812@gmail.com>
2024-01-16 20:49:27 -0500

    added a mail icon to the nav bar

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  assetsInclude: ['**/*.glb'],
   base: "/3d_port_lg/",
-assetsInclude: ['**/*.glb']
 })
